943d659de9e0d6d29ae18ff10243b1ba4c386f44
BuildTools <petadj88@gmail.com>
2021-10-18 21:51:57 +0200

    Reponse Update2 + Seo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
 import * as styles from '../styles/navbar.module.css'
-import { HiMenuAlt3 } from '@react-icons/all-files/hi/HiMenuAlt3'
 import Links from './Links'
-import MobileLinks from './MobileLinks'
+
 
 export default function Navbar({ items }) {
-    const [MenuToggle, setMenuToggle] = useState(false)
-    const HandleMenuToggle = () => {
-        if(MenuToggle)
-        {
-            setMenuToggle(false)
-        } else {
-            setMenuToggle(true)
-        }
-    }
+
     return (
         <div className={styles.nav}>
             <h3>MCJabko.cz</h3>
             <Links items={items} />
-            <MobileLinks toggle_menu={MenuToggle} items={items} />
-            <a role="button" onClick={() => HandleMenuToggle()} aria-label="menu-btn" className={styles.menu_btn}><HiMenuAlt3/></a>
         </div>
     )
 }
