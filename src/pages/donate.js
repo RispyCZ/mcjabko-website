a4800cdce7f5ef07c413422a7f9b578d2b9bbe35
RispyCZ <petadj88@gmail.com>
2021-12-06 20:34:32 +0100

    Donate update

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -13,7 +13,10 @@ export default function donate() {
             </div>
             <div className={styles.content_wrapper}>
                 <p>Náš sever není P2W (Pay to win), proto zde máme pouze rank sponzor.</p>
-                <h3>Výhody</h3>
+
+                <div className={styles.section_top}>
+                    <h2>Výhody</h2>
+                </div>
                 <ul>
                     <li>/kit sponzor-který obsahuje 16 steaků, 5 zlatých ingotů 16 rachejtlí a 2 zlatá jablka. Kit můžeš použít jednou za 24 hodiny</li>
                     <li>/hat-přidá blok který právě držíš v ruce na tvoji hlavu</li>
@@ -28,6 +31,16 @@ export default function donate() {
                     <li>Zlatý prefix v chatu a zlatý nick v tabu.</li>
                     <li>Náš neskonalý vděk</li>
                 </ul>
+                <div className={styles.section_top}>
+                    <h2>Platba pomocí Paypal, Převod na účet</h2>
+                </div>
+                <div className={styles.section}>
+                    <p>Kontaktuj nás prosím na <a href="https://discord.mcjabko.cz">discoru</a></p>
+                </div>
+
+                <div className={styles.section_top}>
+                    <h2>Platba pomocí SMS</h2>
+                </div>
                 <div className={styles.list}>
                     <div className={styles.sponzor_box}>
                         <h3>Sponzor 90 dnů</h3>
@@ -35,6 +48,7 @@ export default function donate() {
                         <p>Odešlete sms ve tvaru:</p>
                         <p><b>PM MCJABKO NICK</b> na číslo <b>9033399</b></p>
                         <p className={styles.red}>Pouze CZ</p>
+                        <p className={styles.small}>Cena SMS je 99Kč vč. DPH</p>
                     </div>
                     <div className={styles.sponzor_box}>
                         <h3>Sponzor 30 dnů</h3>
@@ -42,6 +56,7 @@ export default function donate() {
                         <p>Odešlete sms ve tvaru:</p>
                         <p><b>PM MCJABKO NICK</b> na číslo <b>9033350</b></p>
                         <p className={styles.red}>Pouze CZ</p>
+                        <p className={styles.small}>Cena SMS je 50Kč vč. DPH</p>
                     </div>
                 </div>
                 <span className={styles.small}>Službu technicky zajišťuje Airtoy a.s. Reklamace plateb na reklamace@airtoy.cz  nebo lince 602 777 555, 9 - 17 hodin, Po-Pá, www.platmobilem.cz. Kontakt na provozovatele:rispy@mcjabko.cz</span>
